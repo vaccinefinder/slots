bce103bbe1cdeafe6f80b135de7516c19634251c
vipin <vipin@vipins-MacBook-Air.local>
2021-05-23 11:45:32 +0530

    Updates

---
+++ precache-manifest.5751f4d269dc437e93c50dff69213fad.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c382014ade805c8520c3cf1c4ee9ed9",
+    "url": "/slots/index.html"
+  },
+  {
+    "revision": "1596ac84652d1aa81ad8",
+    "url": "/slots/static/css/2.8e6d4d67.chunk.css"
+  },
+  {
+    "revision": "0fed49dfd54cd974d7a2",
+    "url": "/slots/static/css/main.9f19b21c.chunk.css"
+  },
+  {
+    "revision": "1596ac84652d1aa81ad8",
+    "url": "/slots/static/js/2.9dc4da64.chunk.js"
+  },
+  {
+    "revision": "0fed49dfd54cd974d7a2",
+    "url": "/slots/static/js/main.c91d6eb3.chunk.js"
+  },
+  {
+    "revision": "e00f901e26e9483c51e0",
+    "url": "/slots/static/js/runtime~main.c98d6dd8.js"
+  },
+  {
+    "revision": "0bd3827ce211051e300b1d97bfae08fb",
+    "url": "/slots/static/media/alert.0bd3827c.mp3"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/slots/static/media/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/slots/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/slots/static/media/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/slots/static/media/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/slots/static/media/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/slots/static/media/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/slots/static/media/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/slots/static/media/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/slots/static/media/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/slots/static/media/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/slots/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/slots/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/slots/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/slots/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/slots/static/media/fa-solid-900.e8a427e1.woff2"
+  }
+]);
